d9718023a6f7d68efead6df66b53ce23b993b9c6
Davhabibi <83805742+Davhabibi@users.noreply.github.com>
2022-01-03 01:03:49 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,26 @@
 <?php 
- 
-session_start();
- 
-if (!isset($_SESSION['username'])) {
-    header("Location: index.php");
-}
- 
+ @ob_start();
+	session_start();
+	if(isset($_POST['proses'])){
+		require 'config.php';
+			
+		$user = strip_tags($_POST['user']);
+		$pass = strip_tags($_POST['pass']);
+
+		$sql = 'select member.*, login.user, login.pass
+				from member inner join login on member.id_member = login.id_member
+				where user =? and pass = md5(?)';
+		$row = $config->prepare($sql);
+		$row -> execute(array($user,$pass));
+		$jum = $row -> rowCount();
+		if($jum > 0){
+			$hasil = $row -> fetch();
+			$_SESSION['admin'] = $hasil;
+			echo '<script>alert("Login Sukses");window.location="index.php"</script>';
+		}else{
+			echo '<script>alert("Login Gagal");history.go(-1);</script>';
+		}
+	}
 ?>
  
 <!DOCTYPE html>
@@ -13,20 +28,37 @@ if (!isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
- 
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Berhasil Login</title>
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword">
+
+    <title>Login To Admin</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+        
+    <!-- Custom styles for this template -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style-responsive.css" rel="stylesheet">
 </head>
-<body>
-    <div class="container-logout">
-        <form action="" method="POST" class="login-email">
-            <?php echo "<h1>Selamat Datang, " . $_SESSION['username'] ."!". "</h1>"; ?>
-             
-            <div class="input-group">
-            <a href="logout.php" class="btn">Logout</a>
-            </div>
-        </form>
+<body style="background:#004643;color:#fff;>
+    <div id="login-page" style="padding-top:3pc;">
+     <div class="container">
+		      <form class="form-login" method="POST">
+		        <h2 class="form-login-heading">Aplikasi POS</h2>
+		        <div class="login-wrap">
+		            <input type="text" class="form-control" name="user" placeholder="User ID" autofocus>
+		            <br>
+		            <input type="password" class="form-control" name="pass" placeholder="Password">
+		            <br>
+		            <button class="btn btn-primary btn-block" name="proses" type="submit"><i class="fa fa-lock"></i> SIGN IN</button>
+		        </div>
+		      </form>	  		  	
+	  	 </div>
     </div>
+   <script src="assets/js/jquery.js"></script>
+   <script src="assets/js/bootstrap.min.js"></script>                                                                                                   
 </body>
 </html>
