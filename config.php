476d94e07cf57bad865a1ddfcbc24b0528ad7df0
Davhabibi <83805742+Davhabibi@users.noreply.github.com>
2022-01-03 09:49:57 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,20 @@
 <?php 
+
+date_default_timezone_set("Asia/Jakarta");
+error_reporting(0);
  
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "db_toko";// nama database anda
- 
-try{
-		$config = new PDO("mysql:host=$host;dbname=$dbname;", $user,$pass);
-		//echo 'sukses';
+	$server = "localhost";
+	$user = "root";
+	$pass = "";
+	$database = "db_toko";// nama database anda
+
+	try{
+			$config = new PDO("mysql:host=$host;dbname=$dbname;", $user,$pass);
+			//echo 'sukses';
 	}catch(PDOException $e){
-		echo 'Gagal tersambung dengan database.' .$e -> getMessage();
+			echo 'Gagal tersambung dengan database.' .$e -> getMessage();
 	}
-	
+
 	$view = 'fungsi/view/view.php'; // direktori fungsi select data
  
 ?>
